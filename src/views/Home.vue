5dab5c596d6677c72997808a5a4fe92114e439b0
kristijan117 <kristijanopacak117@gmail.com>
2022-02-11 12:33:54 +0100

    https://github.com/varziclea/lea-kristijan-vue-app-template.git

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,36 @@
 <template>
+<div class="pocetna">
 
-    
+<div class="pocetna-container">
+    <a href="#">
+        <p> O NAMA </p>
+    </a>
+</div>
+
+<div class="pocetna-container">
+    <a href="#">
+        <p> PROGRAMI </p>
+    </a>
+</div>
+
+<div class="pocetna-container">
+<a href="#">
+    <p> SAVJETI </p>
+</a>
+</div>
+
+<div class="pocetna-container">
+<a href="#">
+    <p> KONTAKT </p>
+</a>
+</div>
+
+<div class="pocetna-container">
+<a href="#">
+    <p> PRIIJAVI SE </p>
+</a>
+</div>
+
+</div>
+
 </template>
