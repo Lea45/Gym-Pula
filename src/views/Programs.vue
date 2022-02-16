ee7241a080c2d0f6ad981d989c44604a51825b36
varziclea <lvarzic@student.unipu.hr>
2022-02-16 23:57:51 +0100

    https://github.com/varziclea/lea-kristijan-vue-app-template.git

---
+++ src/views/Programs.vue
@@ -0,0 +1,71 @@
+<template>
+    
+ <div class="container">
+        <div class="card">
+            <div class="content">
+                <div class="contentBx">
+                    <h3>CIRKULARNI POLUSATNI PROGRAM<br><span>kod kuće</span><br><span>300 kn</span></h3>
+                </div>
+            </div>
+            <ul class="sci">
+                <li>
+                    <a href="#">VIDI VIŠE</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="card">
+            <div class="content">
+                <div class="contentBx">
+                    <h3>STANDARDNI PROGRAM ZA CIJELO TIJELO<br><span>teretana</span><br><span>350 kn</span></h3>
+                </div>
+            </div>
+            <ul class="sci">
+                <li>
+                    <a href="#">VIDI VIŠE</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="card">
+            <div class="content">
+                <div class="contentBx">
+                    <h3>GLUTEUS PROGRAM<br><span>teretana</span><br><span>250 kn</span></h3>
+                </div>
+            </div>
+            <ul class="sci">
+                <li>
+                    <a href="#">VIDI VIŠE</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="card">
+            <div class="content">
+                <div class="contentBx">
+                    <h3>STANDARDNI PROGRAM ZA CIJELO TIJELO<br><span>kod kuće</span><br><span>300 kn</span></h3>
+                </div>
+            </div>
+            <ul class="sci">
+                <li>
+                    <a href="#">VIDI VIŠE</a>
+                </li>
+            </ul>
+        </div>
+        
+        <div class="card">
+            <div class="content">
+                <div class="contentBx">
+                    <h3>GLUTEUS PROGRAM<br><span>kod kuće</span><br><span>200 kn</span></h3>
+                </div>
+            </div>
+            <ul class="sci">
+                <li>
+                    <a href="#">VIDI VIŠE</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+
+</template>
