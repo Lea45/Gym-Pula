1d4d4c45c1774d45ec23af8d36721d46980d5f0e
varziclea <lvarzic@student.unipu.hr>
2022-02-24 19:12:32 +0100

    https://github.com/varziclea/lea-kristijan-vue-app-template.git

---
+++ src/views/About.vue
@@ -0,0 +1,51 @@
+<template>
+
+<div class="about"> 
+
+    <ul>
+  <li>
+    <a href="#" contenteditable>
+      <h2>Što?</h2>
+      <p>Profesionalne usluge fitnessa i grupnih treninga u vrhunsko opremljenim prostorijama</p>
+    </a>
+  </li>
+
+  <li>
+    <a href="#" contenteditable>
+      <h2>Tko?</h2>
+      <p>Dva fitness trenera - Kristijan i Lea</p> <br> 
+      <p>250 trenutno aktivnih članova</p>
+    </a>
+  </li>
+
+  <li>
+    <a href="#" contenteditable>
+      <h2>Kako?</h2>
+      <p>Ovisno o tome kakve rezultate želite postići usmjeravamo vas na određene treninge</p>
+    </a>
+  </li>
+
+  <li>
+    <a href="#" contenteditable>
+      <h2>Gdje?</h2>
+      <p>Programe koje kupite možete koristiti kod kuće ili u teretani</p>
+    </a>
+  </li>
+
+  <li>
+    <a href="#" contenteditable>
+      <h2>Prehrana?</h2>
+      <p>Prehrana je vrlo važna! Izrađujemo individualne planove prehrane po vašim potrebama!</p>
+    </a>
+  </li>
+
+  <li>
+    <a href="#" contenteditable>
+      <h2>Prijava</h2>
+      <p>Prijavi se još danas i dovedi svoje tijelo do savršenstva!</p>
+    </a>
+  </li>
+</ul>
+</div>
+
+</template>
