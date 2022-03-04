907eeef376224f4d0939e42c2ea0de032aba3478
varziclea <lvarzic@student.unipu.hr>
2022-03-04 14:30:49 +0100

    https://github.com/varziclea/lea-kristijan-vue-app-template.git

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,13 @@
 <template>
+<div class="contact">
     <div class="image-container">
+        <div class="text">ZAJEDNO DO REZULTATA!</div>
+        <div class="text">Kako nas možeš pronaći?</div>
         <div class="text">Pula - Zagrebačka 112</div>
+        <div class="text">E-mail: info@gympula.hr</div>
+        <div class="text">Telefonski broj: 0991237654</div>
     </div>
+
+    
+</div>
 </template>
