56d1318aaed77d25370b3959bec482a66bd96c6a
kristijan117 <kristijanopacak117@gmail.com>
2022-06-21 22:26:18 +0200

    https://github.com/varziclea/lea-kristijan-vue-app-template.git

--- src/views/Advicess.vue
+++ src/views/Advicess.vue
@@ -1,150 +1,194 @@
 <template>
-<div class="advicess">
+  <div class="advicess">
     <div class="tab">
-  <button class="tablinks" onclick="openTab(event, 'Kontinuitet')">KONTINUITET</button>
-  <button class="tablinks" onclick="openTab(event, 'Prehrana')">PREHRANA</button>
-  <button class="tablinks" onclick="openTab(event, 'Trening')">TRENING</button>
-</div>
-
-<!-- ------------------------------------------------------ Kontinuitet ---------------------------------------------------------------->
-<div id="Kontinuitet" class="tabcontent">
-  <h3>Kontinuitet</h3>
-  <ul class="tips">
-            <li>
-            <b> Najbitniji je kontinuitet! </b> Znanstveno je dokazano da su tjedno 
-            potrebna 3 treninga da osoba kroz određeni period vidi zdravstvene benefite, 
-            a isto se podrazumijeva i za rezultate što se izgleda tiče. NE 3 treninga ovaj tjedan, 
-            pa sljedeći tjedan 2 treninga, pa tjedan iza 1 ili 2 treninga. Svaki tjedan minimalno 
-            3 treninga! Toga se pridržavaj i rezultati su zagarantirani.
-            </li>
-        </ul>
-</div>
-
-
-
-
-<!-- ------------------------------------------------------ Prehrana ---------------------------------------------------------------->
-<div id="Prehrana" class="tabcontent">
-  <h3>Prehrana</h3>
-        <ul class="tips">
-            <li>
-                <b>Prehrana je vrlo bitna.</b> Ni 5 treninga tjedno neće dati odlične rezultate ako kiksaš s hranom. Kiksanje znači:
-                <br>
-                <b>Premali unos proteina. </b> Zašto su proteini važni? ”Znanstveno je potvrđeno da veća količina proteina potiče 
-                topljenje masti na tri načina: smanjenjem apetita, ubrzavanjem metabolizma i održavanjem mišićne mase koja 
-                je izvrstan potrošač kalorija.” Ne, proteini vas neće napuhati, niti ćete se jedno jutro probuditi sa previše mišića.
-                <br>
-                Preporučeni unos proteina za osobe koje su fizički aktivne kreće se od oko 1 g/kg do 2 g/kg. Ukoliko radiš na povećanju 
-                mišićne mase, unos proteina penje se i do 2.5 g/kg. 
-                <br>
-                Proteinske namirnice su: meso (piletina, puretina, crveno meso), riba, posni sir, pureća/pileća šunka, whey protein,
-                 bjelanjak. To su proteini životinjskog porijekla. Proteini iz biljnog porijekla su: iz soje, graška, graha, prokulice, 
-                 leće, šampinjona itd. 
-                <br>
-                 Proteine uključite u svaki obrok, na taj ćete način lakše dostići željeni dnevni unos.
-            </li>
-        </ul>
-
-        <ul class="tips">
-            <li>
-                <b> Premali kalorijski unos. </b> Mnoge žene nisu ni svjesne, ali kada krenu sa dijetom dnevni kalorijski unos jedva dostiže brojku 
-                od 1000 kcal. Takvim velikim kalorijskim deficitom i na duže vrijeme metabolizam se usporava! Time puno sporije dolazi do 
-                gubitka kilograma, a i mogu se dogoditi i oni najgori problemi- problemi sa zdravljem. Izračunajte si koliko kalorija dnevno 
-                trebate unositi da postignete svoj cilj i jedan period brojite koliko jedete. Žena ne bi trebala unositi manje od 1200 kcal dnevno! 
-                Ovdje je jednostavan kalkulator za izračun kalorija: http://www.freebmrcalculator.com/
-            </li>
-        </ul>
-
-        <ul class="tips">
-            <li>
-                <b>Preveliki kalorijski unos. </b> Isto tako, puno žena dobije apetit kada krene sa treninzima, što je super ako se hrane zdravo, međutim, 
-                ako nekontrolirano jedu sve što im dođe pod ruku, završe s viškom kilograma ili masnim naslagama na neželjenim mjestima (budući da nas 
-                masne naslage ne pitaju gdje se trebaju smjestiti).
-                <br>
-                Zato obavezno klikni na link iznad, upiši svoje podatke i ispod ćeš dobiti rezultat kalorija koje moraš unositi ako želiš smršaviti, 
-                udebljati ili ako želiš ostati na istoj kilaži.
-            </li>
-        </ul>
-
-        <ul class="tips">
-            <li> 
-                <b>Česti junk food.</b> Pod time mislim na puno slatkiša, grickalica, sokova, alkohola, fast food-a poput pizze, hamburgera, sendviča i sl. 
-                Takve namirnice konzumirati što rjeđe, najbolje u potpunosti izbaciti u prva dva tjedna, zatim imati jedan do dva takva obroka tjedno.
-            </li>
-        </ul>
-
-        <ul class="tips">
-            <li>
-               <b>Premali unos tekućine. </b>Tijelu je potrebna voda kako bi češće mokrili i izbacivali štetne toksine. Smanjenim unosom tekućine tijelo postojeću 
-                vodu zadržava, izgledamo lošije i ”vodeno”. Osim obične vode koja bi trebala biti glavni izvor hidracije, preporučamo i šalicu biljnog čaja dnevno 
-                (zeleni čaj, čaj od koprive i sl.), čašu svježe cijeđenog soka od naranče ili limunadu bez dodatnih šećera. Dnevno unosi minimalno 2 l tekućine!
-            </li>
-        </ul>
-</div>
-
-
-
-
-<!-- ------------------------------------------------------ Trening ---------------------------------------------------------------->
-<div id="Trening" class="tabcontent">
-  <h3>Trening</h3>
-
-        <ul class="tips">
-            <li> Slijedi nekoliko savjeta što se vježbi i treninga općenito tiče. <br>
-                 <b>Ukoliko si početnik </b>, a zadan broj serija i ponavljanja ti je previše- u prva dva tjedna smanji svaku vježbu za jednu seriju ili za nekoliko ponavljanja. 
-                Zapisuj koliko si napravio/la i na taj način prati napredak.
-                <br>
-                Što se kilaža tiče- bilo koji trener ne bi smio u online programu klijentu odrediti težinu s kojom radi vježbu. To se jedino ispravno može odrediti na 
-                privatnom treningu (ako osobno dođeš vježbati u našu teretanu). Kako bi najbolje odredio/la kilažu s kojom radiš, vježba mora biti izazovna! 
-                Odradiš li 10 ili 12 ponavljanja bez problema i osjetiš da možeš još- kilaža nije dovoljno dobra i izazovna za tebe.
-                <br>
-                Uzmimo za primjer vježbu iskoraci. Prvu seriju si odradio/la s dvije bučice od 3 kg i 20 ponavljanja (sa svakom nogom 10 x) ti je bilo prelagano. 
-                Drugu seriju uzmi dvije bučice od 4 kg. Treću seriju ostavi bučice od 4 kg ili uzmi od 5 kg ukoliko osjećaš da možeš. Dakle ne trebaš sve serije odraditi 
-                s istom kilažom, dapače, neka trening bude izazovan.
-                <br>
-                Ako je vježba hip thrust i prva serija od 12x bila prelagana, iduću seriju dodaj još kila na šipku. Uvijek zapiši kilažu s kojom radiš da znaš za sljedeći 
-                trening.
-                <br>
-                Neke se vježbe ne mogu raditi s većim kilažama i zato služe za dodatno umaranje mišića na kraju treninga radeći ih u puno ponavljanja, tzv. burnout session.
-                <br>
-                Početnici- polako i sigurno prvih mjesec dana! Ne gurajte na sebe kilaže, važnije je vježbu izvoditi pravilno!</li>
-        </ul>
-
-        <ul class="tips">
-            <li>
-                <b>Ukoliko si napredan vježbač/ica </b>, umjesto serija ili broja ponavljanja, predlažem ti da povećaš kilažu s kojom radiš. 
-                <br>
-                Naravno, sve dok vježbu radiš ispravno!
-            </li>
-        </ul>
-
-        <ul class="tips">
-            <li>
-                <b>Zagrijavanje prije treninga! </b>Pravilo je da se prije treninga s utezima zagriješ na način da odradiš par vježbica samo vlastitim tijelom, bez kilaža.
-            </li>
-        </ul>
-
-        <ul class="tips">
-            <li>
-               <b>Za one koji žele više </b> ovisno o svojim ciljevima, u tjednu prakticiraj i nekoliko cardio treninga. 
-               Predlažem trčanje ili brzo hodanje na traci ili u prirodi, bicikliranje, orbitrek. Kada želiš održati masu, ali ipak raditi cardio zbog zdravlja i kondicije
-                - to je najčešće 1 trening tjedno, a ostalo su treninzi snage. Kada želiš smršaviti, cardio radiš 2 ili 3x tjedno po minimalno 30 min + treninzi snage. 
-                Cardio trening je najbolje raditi nakon treninga snage ili zasebno (npr. cardio ujutro, trening snage navečer).
-            </li>
-        </ul>
-
-        <ul class="tips">
-            <li>
-               <b>Odmor </b>, neki ga ne koriste uopće (treniraju previše i često), a neki ga koriste i prečesto pa rezultati izostanu. 
-               Naša je preporuka za početnike da prva dva tjedna rade 2-3 treninga ovisno o tome kako se osjećaju i kakve upale mišića imaju (često jake upale, pa je 
-               potrebno odmoriti koji dan više). Nakon toga treninge mogu raditi i do 5x tjedno. 2 dana odmora. 
-            </li>
-        </ul>
+      <button class="tablinks" onclick="openTab(event, 'Kontinuitet')">
+        KONTINUITET
+      </button>
+      <button class="tablinks" onclick="openTab(event, 'Prehrana')">
+        PREHRANA
+      </button>
+      <button class="tablinks" onclick="openTab(event, 'Trening')">
+        TRENING
+      </button>
     </div>
-</div>
 
+    <!-- ------------------------------------------------------ Kontinuitet ---------------------------------------------------------------->
+    <div id="Kontinuitet" class="tabcontent">
+      <h3>Kontinuitet</h3>
+      <ul class="tips">
+        <li>
+          <b> Najbitniji je kontinuitet! </b> Znanstveno je dokazano da su
+          tjedno potrebna 3 treninga da osoba kroz određeni period vidi
+          zdravstvene benefite, a isto se podrazumijeva i za rezultate što se
+          izgleda tiče. NE 3 treninga ovaj tjedan, pa sljedeći tjedan 2
+          treninga, pa tjedan iza 1 ili 2 treninga. Svaki tjedan minimalno 3
+          treninga! Toga se pridržavaj i rezultati su zagarantirani.
+        </li>
+      </ul>
+    </div>
 
+    <!-- ------------------------------------------------------ Prehrana ---------------------------------------------------------------->
+    <div id="Prehrana" class="tabcontent">
+      <h3>Prehrana</h3>
+      <ul class="tips">
+        <li>
+          <b>Prehrana je vrlo bitna.</b> Ni 5 treninga tjedno neće dati odlične
+          rezultate ako kiksaš s hranom. Kiksanje znači:
+          <br />
+          <b>Premali unos proteina. </b> Zašto su proteini važni? ”Znanstveno je
+          potvrđeno da veća količina proteina potiče topljenje masti na tri
+          načina: smanjenjem apetita, ubrzavanjem metabolizma i održavanjem
+          mišićne mase koja je izvrstan potrošač kalorija.” Ne, proteini vas
+          neće napuhati, niti ćete se jedno jutro probuditi sa previše mišića.
+          <br />
+          Preporučeni unos proteina za osobe koje su fizički aktivne kreće se od
+          oko 1 g/kg do 2 g/kg. Ukoliko radiš na povećanju mišićne mase, unos
+          proteina penje se i do 2.5 g/kg.
+          <br />
+          Proteinske namirnice su: meso (piletina, puretina, crveno meso), riba,
+          posni sir, pureća/pileća šunka, whey protein, bjelanjak. To su
+          proteini životinjskog porijekla. Proteini iz biljnog porijekla su: iz
+          soje, graška, graha, prokulice, leće, šampinjona itd.
+          <br />
+          Proteine uključite u svaki obrok, na taj ćete način lakše dostići
+          željeni dnevni unos.
+        </li>
+      </ul>
 
+      <ul class="tips">
+        <li>
+          <b> Premali kalorijski unos. </b> Mnoge žene nisu ni svjesne, ali kada
+          krenu sa dijetom dnevni kalorijski unos jedva dostiže brojku od 1000
+          kcal. Takvim velikim kalorijskim deficitom i na duže vrijeme
+          metabolizam se usporava! Time puno sporije dolazi do gubitka
+          kilograma, a i mogu se dogoditi i oni najgori problemi- problemi sa
+          zdravljem. Izračunajte si koliko kalorija dnevno trebate unositi da
+          postignete svoj cilj i jedan period brojite koliko jedete. Žena ne bi
+          trebala unositi manje od 1200 kcal dnevno! Ovdje je jednostavan
+          kalkulator za izračun kalorija: http://www.freebmrcalculator.com/
+        </li>
+      </ul>
+
+      <ul class="tips">
+        <li>
+          <b>Preveliki kalorijski unos. </b> Isto tako, puno žena dobije apetit
+          kada krene sa treninzima, što je super ako se hrane zdravo, međutim,
+          ako nekontrolirano jedu sve što im dođe pod ruku, završe s viškom
+          kilograma ili masnim naslagama na neželjenim mjestima (budući da nas
+          masne naslage ne pitaju gdje se trebaju smjestiti).
+          <br />
+          Zato obavezno klikni na link iznad, upiši svoje podatke i ispod ćeš
+          dobiti rezultat kalorija koje moraš unositi ako želiš smršaviti,
+          udebljati ili ako želiš ostati na istoj kilaži.
+        </li>
+      </ul>
+
+      <ul class="tips">
+        <li>
+          <b>Česti junk food.</b> Pod time mislim na puno slatkiša, grickalica,
+          sokova, alkohola, fast food-a poput pizze, hamburgera, sendviča i sl.
+          Takve namirnice konzumirati što rjeđe, najbolje u potpunosti izbaciti
+          u prva dva tjedna, zatim imati jedan do dva takva obroka tjedno.
+        </li>
+      </ul>
+
+      <ul class="tips">
+        <li>
+          <b>Premali unos tekućine. </b>Tijelu je potrebna voda kako bi češće
+          mokrili i izbacivali štetne toksine. Smanjenim unosom tekućine tijelo
+          postojeću vodu zadržava, izgledamo lošije i ”vodeno”. Osim obične vode
+          koja bi trebala biti glavni izvor hidracije, preporučamo i šalicu
+          biljnog čaja dnevno (zeleni čaj, čaj od koprive i sl.), čašu svježe
+          cijeđenog soka od naranče ili limunadu bez dodatnih šećera. Dnevno
+          unosi minimalno 2 l tekućine!
+        </li>
+      </ul>
+    </div>
+
+    <!-- ------------------------------------------------------ Trening ---------------------------------------------------------------->
+    <div id="Trening" class="tabcontent">
+      <h3>Trening</h3>
+
+      <ul class="tips">
+        <li>
+          Slijedi nekoliko savjeta što se vježbi i treninga općenito tiče.
+          <br />
+          <b>Ukoliko si početnik </b>, a zadan broj serija i ponavljanja ti je
+          previše- u prva dva tjedna smanji svaku vježbu za jednu seriju ili za
+          nekoliko ponavljanja. Zapisuj koliko si napravio/la i na taj način
+          prati napredak.
+          <br />
+          Što se kilaža tiče- bilo koji trener ne bi smio u online programu
+          klijentu odrediti težinu s kojom radi vježbu. To se jedino ispravno
+          može odrediti na privatnom treningu (ako osobno dođeš vježbati u našu
+          teretanu). Kako bi najbolje odredio/la kilažu s kojom radiš, vježba
+          mora biti izazovna! Odradiš li 10 ili 12 ponavljanja bez problema i
+          osjetiš da možeš još- kilaža nije dovoljno dobra i izazovna za tebe.
+          <br />
+          Uzmimo za primjer vježbu iskoraci. Prvu seriju si odradio/la s dvije
+          bučice od 3 kg i 20 ponavljanja (sa svakom nogom 10 x) ti je bilo
+          prelagano. Drugu seriju uzmi dvije bučice od 4 kg. Treću seriju ostavi
+          bučice od 4 kg ili uzmi od 5 kg ukoliko osjećaš da možeš. Dakle ne
+          trebaš sve serije odraditi s istom kilažom, dapače, neka trening bude
+          izazovan.
+          <br />
+          Ako je vježba hip thrust i prva serija od 12x bila prelagana, iduću
+          seriju dodaj još kila na šipku. Uvijek zapiši kilažu s kojom radiš da
+          znaš za sljedeći trening.
+          <br />
+          Neke se vježbe ne mogu raditi s većim kilažama i zato služe za dodatno
+          umaranje mišića na kraju treninga radeći ih u puno ponavljanja, tzv.
+          burnout session.
+          <br />
+          Početnici- polako i sigurno prvih mjesec dana! Ne gurajte na sebe
+          kilaže, važnije je vježbu izvoditi pravilno!
+        </li>
+      </ul>
+
+      <ul class="tips">
+        <li>
+          <b>Ukoliko si napredan vježbač/ica </b>, umjesto serija ili broja
+          ponavljanja, predlažem ti da povećaš kilažu s kojom radiš.
+          <br />
+          Naravno, sve dok vježbu radiš ispravno!
+        </li>
+      </ul>
+
+      <ul class="tips">
+        <li>
+          <b>Zagrijavanje prije treninga! </b>Pravilo je da se prije treninga s
+          utezima zagriješ na način da odradiš par vježbica samo vlastitim
+          tijelom, bez kilaža.
+        </li>
+      </ul>
+
+      <ul class="tips">
+        <li>
+          <b>Za one koji žele više </b> ovisno o svojim ciljevima, u tjednu
+          prakticiraj i nekoliko cardio treninga. Predlažem trčanje ili brzo
+          hodanje na traci ili u prirodi, bicikliranje, orbitrek. Kada želiš
+          održati masu, ali ipak raditi cardio zbog zdravlja i kondicije - to je
+          najčešće 1 trening tjedno, a ostalo su treninzi snage. Kada želiš
+          smršaviti, cardio radiš 2 ili 3x tjedno po minimalno 30 min + treninzi
+          snage. Cardio trening je najbolje raditi nakon treninga snage ili
+          zasebno (npr. cardio ujutro, trening snage navečer).
+        </li>
+      </ul>
+
+      <ul class="tips">
+        <li>
+          <b>Odmor </b>, neki ga ne koriste uopće (treniraju previše i često), a
+          neki ga koriste i prečesto pa rezultati izostanu. Naša je preporuka za
+          početnike da prva dva tjedna rade 2-3 treninga ovisno o tome kako se
+          osjećaju i kakve upale mišića imaju (često jake upale, pa je potrebno
+          odmoriti koji dan više). Nakon toga treninge mogu raditi i do 5x
+          tjedno. 2 dana odmora.
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-
+<style lang="scss">
+@import "@/css/advicess";
+</style>
