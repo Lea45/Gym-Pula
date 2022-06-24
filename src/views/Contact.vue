0f3602a9f4d2ce441bbbdcd24a77019689e1c8b0
kristijan117 <kristijanopacak117@gmail.com>
2022-06-24 19:57:24 +0200

    https://github.com/varziclea/lea-kristijan-vue-app-template.git

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,13 +1,15 @@
 <template>
-<div class="contact">
+  <div class="contact">
     <div class="image-container">
-        <div class="text">ZAJEDNO DO REZULTATA!</div>
-        <div class="text">Kako nas možeš pronaći?</div>
-        <div class="text">Pula - Zagrebačka 112</div>
-        <div class="text">E-mail: info@gympula.hr</div>
-        <div class="text">Telefonski broj: 0991237654</div>
+      <div class="text">ZAJEDNO DO REZULTATA!</div>
+      <div class="text">Kako nas možeš pronaći?</div>
+      <div class="text">Pula - Zagrebačka 112</div>
+      <div class="text">E-mail: info@gympula.hr</div>
+      <div class="text">Telefonski broj: 0991237654</div>
     </div>
-
-    
-</div>
+  </div>
 </template>
+
+<style lang="scss">
+@import "@/css/contact";
+</style>
