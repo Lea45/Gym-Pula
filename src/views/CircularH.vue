6784d2a48e928cc5b39db3f043f3e63f87204671
Lea Varžić <varzic.lea@gmail.com>
2022-06-24 09:55:24 +0200

    https://github.com/varziclea/lea-kristijan-vue-app-template.git

--- src/views/CircularH.vue
+++ src/views/CircularH.vue
@@ -1,22 +1,199 @@
 <template>
-    <div class="containter">
-        <div class="trening"> 
-            <h1>CIRKULARNI POLUSATNI PROGRAM </h1>
-            <h3>(kod kuće)</h3>
-            <p> • cilj: mršavljenje i oblikovanje, bolja kondicija i izdržljivost </p>
-            <p> • treninzi traju otprilike pola sata </p>
-            <p> • 5 treninga tjedno za cijelo tijelo: 2 za noge i gluteus, 3 za trbuh, ruke i leđa kod kuće </p>
-            <p> • potrebni rekviziti: bučice (od minimalno 3 kg), fit band, vijača </p>
-            <p> • video svake vježbe pojedinačno (Kristijan i Lea demonstriraju) sa objašnjenjem pravilnog izvođenja </p>
-            <p> • savjeti za najbolje rezultate </p>
-            <p> • program sadržava i upute za zagrijavanje prije svakog treninga te istezanje nakon. </p>   
+  <div class="containter">
+    <div class="trening">
+      <h1>CIRKULARNI POLUSATNI PROGRAM</h1>
+      <h3>(kod kuće)</h3>
+      <p>• cilj: mršavljenje i oblikovanje, bolja kondicija i izdržljivost</p>
+      <p>• treninzi traju otprilike pola sata</p>
+      <p>
+        • 5 treninga tjedno za cijelo tijelo: 2 za noge i gluteus, 3 za trbuh,
+        ruke i leđa kod kuće
+      </p>
+      <p>• potrebni rekviziti: bučice (od minimalno 3 kg), fit band, vijača</p>
+      <p>
+        • video svake vježbe pojedinačno (Kristijan i Lea demonstriraju) sa
+        objašnjenjem pravilnog izvođenja
+      </p>
+      <p>• savjeti za najbolje rezultate</p>
+      <p>
+        • program sadržava i upute za zagrijavanje prije svakog treninga te
+        istezanje nakon.
+      </p>
 
-            <br> 
-            <ul class="login">
-                <li>
-                    <a href="">DODAJ U KOŠARICU!</a>
-                </li>
-            </ul>  
+      <br />
+      <ul class="login">
+        <li>
+          <button
+            onclick="document.getElementById('id03').style.display='block'"
+          >
+            DODAJ U KOŠARICU!
+          </button>
+        </li>
+      </ul>
+    </div>
+    <div id="id03" class="modal">
+      <form
+        class="modal-content animate"
+        action="/action_page.php"
+        method="post"
+      >
+        <!-- ---------------------------------------------------------- close modal (x) ---------------------------------------------------------------->
+        <div class="imgcontainer">
+          <span
+            onclick="document.getElementById('id03').style.display='none'"
+            class="close"
+            title="Close Modal"
+            >&times;</span
+          >
+          <img src="images/login.jpg" alt="dumbell" class="dumbell" />
         </div>
+
+        <!-- ------------------------------------------------------- korisničko ime / e-mail ---------------------------------------------------------------->
+        <div class="container">
+          <label for="uname"><b>E-mail</b></label>
+          <input
+            type="text"
+            v-model="Email"
+            placeholder="Unesi e-mail.."
+            name="uname"
+            required
+          />
+          <br />
+
+          <!-- ---------------------------------------------------------------- lozinka ---------------------------------------------------------------------->
+          <label for="psw"><b>Lozinka</b></label>
+          <input
+            type="password"
+            v-model="Password"
+            placeholder="Unesi lozinku..."
+            name="psw"
+            required
+          />
+          <br />
+
+          <!-- ---------------------------------------------------------------- trening ---------------------------------------------------------------------->
+          <label for="nname"><b>Trening</b></label>
+          <select v-model="odabir" name="nname" placeholder="Unesi vjezbu...">
+            <option value="CIRKULARNI POLUSATNI PROGRAM">
+              CIRKULARNI POLUSATNI PROGRAM
+            </option>
+            <option value="STANDARDNI PROGRAM ZA CIJELO TIJELO">
+              STANDARDNI PROGRAM ZA CIJELO TIJELO
+            </option>
+            <option value="GLUTEUS PROGRAM">GLUTEUS PROGRAM</option>
+          </select>
+
+          <!-- ---------------------------------------------------------------- telefon ---------------------------------------------------------------------->
+          <label for="bname"><b>Broj telefona</b></label>
+          <input
+            type="text"
+            v-model="telefon"
+            placeholder="Unesi broj telefona..."
+            name="bname"
+            required
+          />
+
+          <!-- ---------------------------------------------------------------- prijava ---------------------------------------------------------------------->
+          <button type="button" @click="begin">Prijava</button> <br />
+          <br />
+
+          <!-- ---------------------------------------------------------------- zapamti me ---------------------------------------------------------------------->
+          <!--<label>
+            <input type="checkbox" checked="checked" name="remember" /> Zapamti
+            me
+          </label>-->
         </div>
+
+        <!-- ---------------------------------------------------------------- zatvori ---------------------------------------------------------------------->
+        <div class="container">
+          <button
+            type="button"
+            onclick="document.getElementById('id03').style.display='none'"
+            class="cancelbtn"
+          >
+            Zatvori
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
+
+<style lang="scss">
+@import "@/css/allprograms";
+</style>
+
+<script>
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "@/firebase.js";
+
+export default {
+  name: "Programs",
+  data() {
+    return {
+      Email: "",
+      Password: "",
+      odabir: "CIRKULARNI POLUSATNI PROGRAM",
+      telefon: "",
+    };
+  },
+  methods: {
+    begin() {
+      signInWithEmailAndPassword(auth, this.Email, this.Password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          this.saveData();
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          if (error.code == "auth/user-not-found") {
+            console.log("User does not exist! Creating new user...");
+            this.signup();
+          } else {
+            alert("Krivi email ili lozinka!");
+          }
+        });
+    },
+    signup() {
+      createUserWithEmailAndPassword(auth, this.Email, this.Password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log("User created!");
+          this.saveData();
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+        });
+    },
+    async saveData() {
+      // Add a new document in collection "cities"
+      await setDoc(doc(db, "VJEZBE", this.Email), {
+        telefon: this.telefon,
+        vjezba: this.odabir,
+      });
+      console.log("Data saved.");
+      signOut(auth)
+        .then(() => {
+          // Sign-out successful.
+          console.log("User logged out");
+          alert("Uspješno ste prijavili vježbu!");
+        })
+        .catch((error) => {
+          // An error happened.
+        });
+    },
+  },
+};
+</script>
