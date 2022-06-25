ab7b7fa6cdbbaaa7f629296c7fa8792b9f34c20e
kristijan117 <kristijanopacak117@gmail.com>
2022-06-25 21:38:50 +0200

    https://github.com/varziclea/lea-kristijan-vue-app-template.git

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,127 +2,153 @@
   <div class="gumb-content">
     <h2>Prijavi se!</h2>
 
-<!-- ------------------------------------------------------ Raspored ---------------------------------------------------------------->
+    <!-- ------------------------------------------------------ Raspored ---------------------------------------------------------------->
 
-<table >
-        <tr>
-          <th>Treninzi</th>
-          <th>Ponedjeljak</th>
-          <th>Utorak</th>
-          <th>Srijeda</th>
-          <th>Četvrtak</th>
-          <th>Petak</th>
-          <th>Subota</th>
-        </tr>
+    <table>
+      <tr>
+        <th>Treninzi</th>
+        <th>Ponedjeljak</th>
+        <th>Utorak</th>
+        <th>Srijeda</th>
+        <th>Četvrtak</th>
+        <th>Petak</th>
+        <th>Subota</th>
+      </tr>
 
-        <tr>
-            <td> Cikularni </td>
-            <td> 19:00 </td>
-            <td> 19:00 </td>
-            <td> / </td>
-            <td> 19:00  </td>
-            <td> 19:00  </td>
-            <td> 19:00  </td>
-        </tr>
-        
-        <tr>
-            <td> Standardni </td>
-            <td> 17:00 </td>
-            <td> 17:00 </td>
-            <td> / </td>
-            <td> 17:00 </td>
-            <td> 17:00 </td>
-            <td> / </td>
-        </tr>
+      <tr>
+        <td>Cikularni</td>
+        <td>19:00</td>
+        <td>19:00</td>
+        <td>/</td>
+        <td>19:00</td>
+        <td>19:00</td>
+        <td>19:00</td>
+      </tr>
 
-        <tr>
-            <td> Cijelo tijelo </td>
-            <td> / </td>
-            <td> 09:00 </td>
-            <td> 09:00 </td>
-            <td> / </td>
-            <td> 09:00 </td>
-            <td> 09:00 </td>
-        </tr>
+      <tr>
+        <td>Standardni</td>
+        <td>17:00</td>
+        <td>17:00</td>
+        <td>/</td>
+        <td>17:00</td>
+        <td>17:00</td>
+        <td>/</td>
+      </tr>
 
-        <tr>
-            <td> Gluteus </td>
-            <td> 12:00 </td>
-            <td>  / </td>
-            <td> 12:00 </td>
-            <td> / </td>
-            <td> 12:00 </td>
-            <td> / </td>
+      <tr>
+        <td>Gluteus</td>
+        <td>12:00</td>
+        <td>/</td>
+        <td>12:00</td>
+        <td>/</td>
+        <td>12:00</td>
+        <td>/</td>
       </tr>
     </table>
 
-
     <!-- ---------------------------------------------------------- LOGIN FORM ---------------------------------------------------------------->
 
-    <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Prijava</button>
+    <button
+      onclick="document.getElementById('id01').style.display='block'"
+      style="width: auto"
+    >
+      Prijava
+    </button>
 
     <div id="id01" class="modal">
-  
-      <form class="modal-content animate" action="/action_page.php" method="post">
-
-
-    <!-- ---------------------------------------------------------- close modal (x) ---------------------------------------------------------------->
-        <div class="imgcontainer"> 
-         <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-          <img src="images/login.jpg" alt="dumbell" class="dumbell">
+      <form
+        class="modal-content animate"
+        action="/action_page.php"
+        method="post"
+      >
+        <!-- ---------------------------------------------------------- close modal (x) ---------------------------------------------------------------->
+        <div class="imgcontainer">
+          <span
+            onclick="document.getElementById('id01').style.display='none'"
+            class="close"
+            title="Close Modal"
+            >&times;</span
+          >
+          <img src="images/login.jpg" alt="dumbell" class="dumbell" />
         </div>
 
-
-
-    <!-- ------------------------------------------------------- korisničko ime / e-mail ---------------------------------------------------------------->
+        <!-- ------------------------------------------------------- korisničko ime / e-mail ---------------------------------------------------------------->
         <div class="container">
           <label for="uname"><b>E-mail</b></label>
-          <input type="text" v-model="Email" placeholder="Unesi e-mail.." name="uname" required> <br>
+          <input
+            type="text"
+            v-model="Email"
+            placeholder="Unesi e-mail.."
+            name="uname"
+            required
+          />
+          <br />
 
-
-
-      <!-- ---------------------------------------------------------------- lozinka ---------------------------------------------------------------------->
+          <!-- ---------------------------------------------------------------- lozinka ---------------------------------------------------------------------->
           <label for="psw"><b>Lozinka</b></label>
-          <input type="password" v-model="Password" placeholder="Unesi lozinku..." name="psw" required> <br>
+          <input
+            type="password"
+            v-model="Password"
+            placeholder="Unesi lozinku..."
+            name="psw"
+            required
+          />
+          <br />
 
-
-      <!-- ---------------------------------------------------------------- trening ---------------------------------------------------------------------->
+          <!-- ---------------------------------------------------------------- trening ---------------------------------------------------------------------->
           <label for="nname"><b>Trening</b></label>
-          <input type="text" v-model="odabir" placeholder="Unesi trening za koji se želis prijaviti..." name="nname" required>
+          <select v-model="odabir" name="nname" placeholder="Unesi vjezbu...">
+            <option value="CIRKULARNI POLUSATNI PROGRAM">
+              CIRKULARNI POLUSATNI PROGRAM
+            </option>
+            <option value="STANDARDNI PROGRAM ZA CIJELO TIJELO">
+              STANDARDNI PROGRAM ZA CIJELO TIJELO
+            </option>
+            <option value="GLUTEUS PROGRAM">GLUTEUS PROGRAM</option>
+          </select>
 
-
-      <!-- ---------------------------------------------------------------- telefon ---------------------------------------------------------------------->
+          <!-- ---------------------------------------------------------------- telefon ---------------------------------------------------------------------->
           <label for="bname"><b>Broj telefona</b></label>
-          <input type="text" v-model="telefon" placeholder="Unesi broj telefona..." name="bname" required>
+          <input
+            type="text"
+            v-model="telefon"
+            placeholder="Unesi broj telefona..."
+            name="bname"
+            required
+          />
 
+          <!-- ---------------------------------------------------------------- prijava ---------------------------------------------------------------------->
+          <button type="button" @click="begin">Prijava</button> <br />
+          <br />
 
-      <!-- ---------------------------------------------------------------- prijava ---------------------------------------------------------------------->
-          <button type="button" @click="signup">Prijava</button> <br> <br>
+   
+        </div>
 
-        
-
-      <!-- ---------------------------------------------------------------- zapamti me ---------------------------------------------------------------------->
-          <label>
-            <input type="checkbox" checked="checked" name="remember"> Zapamti me
-          </label>
-
+        <!-- ---------------------------------------------------------------- zatvori ---------------------------------------------------------------------->
+        <div class="container">
+          <button
+            type="button"
+            onclick="document.getElementById('id01').style.display='none'"
+            class="cancelbtn"
+          >
+            Zatvori
+          </button>
+        </div>
+      </form>
     </div>
-
-
-      <!-- ---------------------------------------------------------------- zatvori ---------------------------------------------------------------------->
-    <div class="container">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Zatvori</button>
-    </div>
-
-  </form>
-
-    </div>
-</div>
-
+  </div>
 </template>
 
 <script>
-import { firebase } from "@/firebase";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { app, auth, db } from "@/firebase.js";
+
 export default {
   name: "Login",
   data() {
@@ -134,14 +160,55 @@ export default {
     };
   },
   methods: {
+    begin() {
+      signInWithEmailAndPassword(auth, this.Email, this.Password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          this.saveData();
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          if (error.code == "auth/user-not-found") {
+            console.log("User does not exist! Creating new user...");
+            this.signup();
+          } else {
+            alert("Krivi email ili lozinka!");
+          }
+        });
+    },
     signup() {
-      firebase
-        .auth()
-
-        .createUserWithEmailAndPassword(this.Email, this.Password, this.odabir, this.telefon)
-        alert("UPJEŠNA PRIJAVA!");
-
-        console.log('Nastavak');
+      createUserWithEmailAndPassword(auth, this.Email, this.Password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log("User created!");
+          this.saveData();
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ..
+        });
+    },
+    async saveData() {
+      // Add a new document in collection "cities"
+      await setDoc(doc(db, "VJEZBE", this.Email), {
+        telefon: this.telefon,
+        vjezba: this.odabir,
+      });
+      console.log("Data saved.");
+      signOut(auth)
+        .then(() => {
+          // Sign-out successful.
+          console.log("User logged out");
+          alert("Uspješno ste prijavili vježbu!");
+        })
+        .catch((error) => {
+          // An error happened.
+        });
     },
   },
 };
