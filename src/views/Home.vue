7b4803e1ccbb79e50e1d4f90e353c3a6a40aea5f
kristijan117 <kristijanopacak117@gmail.com>
2022-06-26 21:32:14 +0200

    https://github.com/varziclea/lea-kristijan-vue-app-template.git

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,19 @@
 <template>
+  <div class="images-container">
+    <div class="img">
+      <img src="images/home/gym1.jpeg" />
+    </div>
 
+    <div class="img">
+      <img src="images/home/gym2.jpeg" />
+    </div>
 
-<div class="images-container">
-  <div class="img">
-    <img src="images/home/gym1.jpeg">
+    <div class="img">
+      <img src="images/home/gym3.jpeg" />
+    </div>
   </div>
-
-  <div class="img">
-    <img src="images/home/gym2.jpeg">
-  </div>
-
-  <div class="img">
-    <img src="images/home/gym3.jpeg">
-  </div>
-  
-</div>
-
-
 </template>
+
+<style lang="scss">
+@import "@/css/home";
+</style>
